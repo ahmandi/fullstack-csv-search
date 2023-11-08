0d0e57c6e0bab6ab0e925854ed23252e8db6a739
Amanda <amandard94@gmail.com>
2023-11-07 23:20:41 -0300

    fix: cors policy update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,7 @@ import filesRouter from './routes/files.routes';
 const app = express();
 const port = 3000;
 
-app.use(cors({ origin: 'https://partnersdata-loader.onrender.com' }));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
